420e5d255f8346fed6efff27ccc38708c5bc9f3b
Dane Sherburn <danesherbs@gmail.com>
2020-10-16 18:10:36 +1100

    Add crawler and project dependencies

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject clementine "0.0.1"
+  :description "A tiny web crawler in Clojure!"
+  :main clementine.core
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/core.async "1.3.610"]
+                 [enlive "1.1.6"]
+                 [clj-http "3.10.3"]])
